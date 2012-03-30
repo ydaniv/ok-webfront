cd9e1eb78c4777290f3f37a8e6002fb39cd0095c
Zohar Arad <zohar@zohararad.com>
2012-03-30 17:36:12 +0300

    start work on layout template

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'mustache');
-  app.set('view options',{layout:false});
+  app.set('view options',{layout:true});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
