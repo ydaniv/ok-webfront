eef248ef0ccb0cee13b6879919d870f4ac185295
daonb <bennydaon@gmail.com>
2012-05-13 17:50:26 +0300

    improve member display: order, edges

--- public/js/apps/member.js
+++ public/js/apps/member.js
@@ -7,15 +7,16 @@ define([
     var app = {
         agendas : function (uri) {
             $.get(uri, function (data) {
+                data.agendas.sort(function(x,y) { return y.score-x.score});
                 var template = $("#agenda-template").html();
                 $("#agendas").html(Mustache.render(template, data));
                 for (var i in data.agendas) {
                   var v=data.agendas[i];
                   $("#agenda-score-"+v.id).css('right', (v.score+100)/2+'%');
                   $("#agenda-bar-"+v.id).css('right', (v.min+100)/2+'%');
-                  $("#agenda-bar-"+v.id).css('width', (v.max-v.min)/2+'%');
+                  $("#agenda-bar-"+v.id).css('width', (v.max-v.min+0.5)/2+'%');
                   $("#agenda-bar-party-"+v.id).css('right', (v.party_min+100)/2+'%');
-                  $("#agenda-bar-party-"+v.id).css('width', (v.party_max-v.party_min)/2+'%');
+                  $("#agenda-bar-party-"+v.id).css('width', (v.party_max-v.party_min+0.5)/2+'%');
                 }
             })
          }
