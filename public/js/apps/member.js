a7f4246bd8de55bf70cdf75d2224f4d903c1785a
daonb <bennydaon@gmail.com>
2012-05-13 15:54:28 +0300

    fix the party bar display

--- public/js/apps/member.js
+++ public/js/apps/member.js
@@ -14,8 +14,8 @@ define([
                   $("#agenda-score-"+v.id).css('right', (v.score+100)/2+'%');
                   $("#agenda-bar-"+v.id).css('right', (v.min+100)/2+'%');
                   $("#agenda-bar-"+v.id).css('width', (v.max-v.min)/2+'%');
-                  $("#agenda-bar-party"+v.id).css('right', (v.party_min+100)/2+'%');
-                  $("#agenda-bar-party"+v.id).css('width', (v.party_max-v.party_min)/2+'%');
+                  $("#agenda-bar-party-"+v.id).css('right', (v.party_min+100)/2+'%');
+                  $("#agenda-bar-party-"+v.id).css('width', (v.party_max-v.party_min)/2+'%');
                 }
             })
          }
