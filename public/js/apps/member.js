88dd4d2a3d9c623f75e37d4a7fab9017e5dd29f2
daonb <bennydaon@gmail.com>
2012-05-15 12:04:34 +0300

    adding agenda score legend

--- public/js/apps/member.js
+++ public/js/apps/member.js
@@ -12,11 +12,11 @@ define([
                 $("#agendas").html(Mustache.render(template, data));
                 for (var i in data.agendas) {
                   var v=data.agendas[i];
-                  $("#agenda-score-"+v.id).css('right', (v.score+100)/2+'%');
+                  $("#agenda-score-"+v.id).css('right', (v.score+100-1)/2+'%');
                   $("#agenda-bar-"+v.id).css('right', (v.min+100)/2+'%');
                   $("#agenda-bar-"+v.id).css('width', (v.max-v.min+0.5)/2+'%');
                   $("#agenda-bar-party-"+v.id).css('right', (v.party_min+100)/2+'%');
-                  $("#agenda-bar-party-"+v.id).css('width', (v.party_max-v.party_min+0.5)/2+'%');
+                  $("#agenda-bar-party-"+v.id).css('width', (v.party_max-v.party_min+1)/2+'%');
                 }
             })
          }
